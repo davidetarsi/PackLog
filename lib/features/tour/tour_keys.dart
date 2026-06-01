b1fb1f463684bde20553b14e65ea96352b6b9411
Davide Tarsi <davidetarsi.dev@gmail.com>
2026-06-01 16:01:55 +0200

    feat(tour): add tour GlobalKeys singleton

---
+++ lib/features/tour/tour_keys.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+final tourKeys = _TourKeys();
+
+class _TourKeys {
+  final housesTab = GlobalKey();
+  final tripsTab = GlobalKey();
+  final profileTab = GlobalKey();
+  final infoCardTarget = GlobalKey();
+}
